1c5c009219b669574c475d1d18b1eb89142bcf55
Justin Wong JH <63507306+amidstdebug@users.noreply.github.com>
2021-09-03 23:02:34 +0800

    Update
    Updated:
    1. base.html (added alert)
    2. login.html (added form groups)
    3. signup.html (added form groups)
    4. init.py (added and initialised SQL and databases
    5. auth.py (added form input checking using GET and POST)
    6. models.py (added database creation for User)
    
    What did I learn?
    1. GET and POST
    2. Learnt how to create databases in Python with db.Column
    
    Upcoming Tasks
    1. Finish up models.py and learn more in-depth info about the classes especially the Note class
    
    안녕~

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,34 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
 
-@auth.route('/logout')
+@auth.route('/logout', methods=['GET', 'POST'])
 def logout():
     return render_template('logout.html')
 
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash("Email too short!", category="fail")
+        elif len(name) < 1:
+            flash("Name too short!", category="fail")
+        elif password1 != password2:
+            flash("Password not matching!", category="fail")
+        elif len(password1) < 7:
+            flash("Password too short!", category='fail')
+        else:
+            flash("Account Created!", category="pass")
     return render_template('signup.html')
