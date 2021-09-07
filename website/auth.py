7e427bdeb4ed32fb10564be5cc778f4e57df09fc
Justin Wong JH <63507306+amidstdebug@users.noreply.github.com>
2021-09-08 02:45:07 +0800

    Update #3
    Updated:
    - init.py (Did the login manager to ensure that users are unable to access homepage
      without logging in)
    
    - auth.py (Did the logic for the user login and logout. Also updated logic to check for
       concurrently existing users in the database using user.query.filter_by(). This will
       ensure that the integrity error doesn't occur)
    
    - views.py (Added the @login_required decorator prerequisite to ensure users MUST
       login before accessing the homepage, which kinda makes sense if you think about
       it)
    
    What did I learn?
    - Password hashes are cool
    
    - Always remember to use the correct variable name
    
    - Python Decorators are important, but I am unsure as to how they are used, so I'll
       need to look it up
    
    - request.form.get and user.query.filter_by are important when referencing data
      located in external files
    
    What will I do next?
    - Go and familiarise myself with the whole 'flask_login' login_user, login_required,
       logout_user, current_user functions
    
    - Learn how to use python decorators
    
    - Finish up the remainder of the project

--- website/auth.py
+++ website/auth.py
@@ -2,18 +2,42 @@ from flask import Blueprint, render_template, request, flash, url_for, redirect
 from .models import User, Note
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password1')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash(
+                    'Logged In', category='pass'
+                )
+                login_user(user, remember=True)
+
+                return redirect(url_for('views.home'))
+            else:
+                flash(
+                    'Incorrect password', category='fail'
+                )
+        else:
+            flash(
+                'Invalid Email', category='fail'
+            )
     return render_template('login.html')
 
 
 @auth.route('/logout', methods=['GET', 'POST'])
+@login_required
 def logout():
-    return render_template('logout.html')
+    logout_user()
+    return render_template('auth.login')
 
 
 @auth.route('/signup', methods=['GET', 'POST'])
@@ -24,19 +48,41 @@ def signup():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
-            flash("Email too short!", category="fail")
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash(
+                'Email Exists!', category='fail'
+            )
+
+        elif len(email) < 4:
+            flash(
+                "Email too short!", category="fail"
+            )
         elif len(name) < 1:
-            flash("Name too short!", category="fail")
+            flash(
+                "Name too short!", category="fail"
+            )
         elif password1 != password2:
-            flash("Password not matching!", category="fail")
+            flash(
+                "Password not matching!", category="fail"
+            )
         elif len(password1) < 7:
-            flash("Password too short!", category='fail')
+            flash(
+                "Password too short!", category='fail'
+            )
         else:
-            new_user = User(email=email, name=name, password=generate_password_hash(password1, method='sha256'))
+            new_user = User(
+                email=email,
+                name=name,
+                password=generate_password_hash(password1, method='sha256')
+            )
             db.session.add(new_user)
             db.session.commit()
-            flash("Account Created!", category="pass")
+            login_user(new_user, remember=True)
+            flash(
+                "Account Created!", category="pass"
+            )
             return redirect(url_for('views.home'))
 
     return render_template('signup.html')
